328b9575a4112fc6e399ac8944b2679426cd7e26
IvanR <yu181219710>
2021-06-25 20:12:08 +0500

    first components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import React from "react";
+import Information from "./components/information";
+import Navigation from "./components/navigation";
+import MainSide from "./components/main-side";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navigation/>
+            <MainSide/>
+            <Information/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/main-side/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainSide = () => {
+    return (
+        <article>
+
+        </article>
+    );
+};
+
+export default MainSide;
